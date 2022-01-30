c98bbd0150a9f0befde3b3392072d97d9e4fcdc7
Anthony Gladden <a.gladden360@outlook.com>
2022-01-30 12:24:56 -0800

    made changes to style.css and scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
-  $('form#q')
-}
+  $('form#q').submit(function(event) {
+    function addTogether('choice1, choice2, choice3, choice4, choice5'){
+      return 'choice1, choice2, choice3, choice4, choice5'
+    }
+    $('#results').show()
+
+    event.preventDefault();
+  });
+});
