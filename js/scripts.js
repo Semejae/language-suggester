9dd4c4295d722e1588999bc5690daf414958de07
Anthony Gladden <a.gladden360@outlook.com>
2022-01-30 18:24:28 -0800

    WIP

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,26 @@
 // business logic//
 $(document).ready(function() {
   $('form#q').submit(function(event) {
-    const outcome = $('input:radio[name=choice4]:checked').val();
-    
+    const answer = $('input:radio[value]:checked').val();
+    let outcome = answer
     
 // userface logic//
 
-    if (outcome === 'value1') { 
+    if (outcome === 'html') {
       $('#Result1').show();
       $('#Result2').hide();
       $('#Result3').hide();
-    } else if (outcome === 'value2') {
-      $('#Result2').show();
-      $('#Result3').hide();
-      $('#Result1').hide(); 
-    } else {
-      $('#Result3').show();
-      $('#Result2').hide();
+    } else if (outcome ==='javascript') {
+      $('#Results2').show();
       $('#Result1').hide();
-    }
+      $('#Result3').hide(); 
+    } else if (outcome === 'python') {
+      $('#Result3').show();
+    } 
+    // else {
+    //   alert('please click a bubble!')
+    // }
       
-
     event.preventDefault();
   });
 });
