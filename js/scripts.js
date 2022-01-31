1412d55170f66b3950e28d83c618d1fd96fbdcff
Anthony Gladden <a.gladden360@outlook.com>
2022-01-30 16:19:03 -0800

    WIP

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,24 @@
 $(document).ready(function() {
   $('form#q').submit(function(event) {
-    const result = $('input:radio[name=choice4]:checked').val();
+    let outcome = $('input:radio[value=html]:checked').val();
+    let outcome = $('input')
     
-    
-    if (result === "html,python,javascript") {
-      $(#outcome).show();
+
+    if (outcome) { 
+      
+
+    // if (result === "html") {
+    //   $(#outcome).show();
+    // } else if (result === "python") {
+    //     $(#outcome).show();
+    // } else (result === "javascrpit")
+    //     $(#outcome).show()
+    //   } 
+
+    $('#outcome').text(results)
+    $('#results').show();
+
     }
-    
 
     event.preventDefault();
   });
