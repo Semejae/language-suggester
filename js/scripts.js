d2118260a2e6055e652bbfd000cc914e4457e035
Anthony Gladden <a.gladden360@outlook.com>
2022-02-02 18:44:29 -0800

    added myQuestion,array too scripts and index

--- js/scripts.js
+++ js/scripts.js
@@ -2,31 +2,33 @@
 $(document).ready(function() {
   $('form#questions').submit(function(event) {
     
-    const outcome = $('input:radio[name=choice]:checked').val();
-    
-
+    const myQuestion1 = $('input:radio[name=question1]:checked').val();
+    const myQuestion2 = $('input:radio[name=question2]:checked').val();
+    const myQuestion3 = $('input:radio[name=question3]:checked').val();
+    const myQuestion4 = $('input:radio[name=question4]:checked').val();
+    const myQuestion5 = $('input:radio[name=question5]:checked').val();
     
 // userface logic//
-
-    if(outcome === 'Red') {
+    
+    if(array === '') {
       $('#R1').show();
       $('#R2').hide();
       $('#R3').hide();
-    } else if(outcome === 'Blue') {
+      
+    } else if(array === 'Blue') {
       $('#R2').show();
       $('#R1').hide();
-      $('#R3').hide(); 
-
-    } else if(outcome === 'Orange') {
+      $('#R3').hide();
+      
+    } else if(array === 'Orange') {
       $('#R3').show();
       $('#R2').hide();
       $('#R1').hide();
-
-    } 
-      else {
+      
+    } else {
       alert('please click bubble!!!')
-
     }
+    
     
     
     
